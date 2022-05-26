01ecbc5ed13fbe3661bd9e75ab96d2b402f9bf1e
Kurdy <davidmontalvo211@gmail.com>
2022-05-25 23:30:03 -0600

    prueba 01

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//tab js
+let tabs = document.querySelectorAll(".content-tab-dos p");
+let tabContents = document.querySelectorAll(".content-text-dos div");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabContents.forEach((content) => {
+      content.classList.remove("active");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tabContents[index].classList.add("active");
+    tabs[index].classList.add("active");
+  });
+});
+
+
